88f0db64ccbf27b00509bad9d393b7c09dfde69b
EduardoCassanha <eduardocassanhaperalta@gmail.com>
2026-03-06 12:43:55 -0300

    feat(web): create API service to fetch weather data from backend

--- frontend/script.js
+++ frontend/script.js
@@ -1,3 +1,5 @@
+const API_URL = 'http://127.0.0.1:8000/check-rain';
+
 document.getElementById('btn').addEventListener('click', handleSubmit);
 
 function handleSubmit() {
@@ -11,4 +13,19 @@ function handleSubmit() {
     }
 
     console.log({ origin, destination, departure });
+}
+
+async function fetchWeather(origin, destination, departure) {
+    const body = { origin, destination };
+    if (departure) body.departure_time = departure.slice(0, 16) + ':00';
+
+    const response = await fetch(API_URL, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(body),
+    })
+
+    if (!response.ok) throw new Error('API error');
+
+    return await response.json();
 }
